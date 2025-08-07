bf6c9c5a7e98a3f6d7313ae4eab1ca785276e816
May <mehwish.irv@gmail.com>
2025-08-07 16:27:29 +0200

    Setting Image Map

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const img = document.getElementById("myImage");
 img.addEventListener("click", () => {
     img.classList.toggle("small");
 })
+
+function computer(){
+    alert("You clicked on the computer");
+}
+
+function chair(){
+    alert("You clicked on the chair");
+}
+
+function keyboard(){
+    alert("You clicked on the keyboard");
+}
+
